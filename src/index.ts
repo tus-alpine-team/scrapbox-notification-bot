673898a873970576705310bf03c73fdd8bd5c066
takker99 <37929109+takker99@users.noreply.github.com>
2020-08-29 01:25:24 +0900

    :sparkles: 毎日20:00に最大で一回更新情報を投げるように設定した

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,35 @@ import { cacheURLs, flushURLs } from './cacheURL';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function doPost(e: any): void {
     const data = JSON.parse(e.postData.getDataAsString()) as ScrapboxNotifyData;
-    const hr = '------------';
 
     // 更新されたページのURLを取得する
     const urls = data.attachments.map((notice) => notice.title_link);
     cacheURLs(urls);
+}
+
+// 更新情報をLINEに投げる
+function postMessage(): void {
+    const urls = flushURLs();
+    if (urls.length == 0) return;
+    const hr = '\n------------';
     const result = [
         hr,
         '本日は以下の記事が更新されました。',
-        ...flushURLs().map((url) => ` ･ ${url}`),
+        ...urls.map((url) => ` ･ ${url}`),
         '詳細は https://scrapbox.io/stream/tus-alpine/ を御覧ください',
     ];
     sendToLine(result.join('\n'));
+}
+
+// 20:00に更新情報を投げるように設定する
+function setTrigger(): void {
+    //作成済みのtriggerがあったら全て消す
+    ScriptApp.getProjectTriggers()
+        .filter((trigger) => trigger.getHandlerFunction() == 'postMessage')
+        .forEach((trigger) => ScriptApp.deleteTrigger(trigger));
+
+    const now = new Date();
+    now.setHours(20);
+    now.setMinutes(0);
+    ScriptApp.newTrigger('postMessage').timeBased().at(now).create();
 }
